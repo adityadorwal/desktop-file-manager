6dc2f3a48510c37b818a49f10e6fa15593336615
Aditya Dorwal <157326050+adityadorwal@users.noreply.github.com>
2026-03-10 19:10:53 +0530

    Remove microphone listening on GUI launch
    Removed microphone click functionality from GUI launch.

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 from core.gui import launch_gui
-from core.voice import listen_and_print
-
-def on_mic_click():
-    listen_and_print()
 
 if __name__ == "__main__":
-    launch_gui(on_mic_click)
+    launch_gui()
